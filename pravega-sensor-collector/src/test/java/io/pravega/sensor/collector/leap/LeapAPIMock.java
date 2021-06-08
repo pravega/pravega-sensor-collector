d1779a93e23891e2e2107c333b94a274779cfe84
apoorva918 <apoorva.shivpuriya918@gmail.com>
2021-06-08 14:35:10 -0500

    mock leap http server working

---
+++ pravega-sensor-collector/src/test/java/io/pravega/sensor/collector/leap/LeapAPIMock.java
@@ -0,0 +1,42 @@
+package io.pravega.sensor.collector.leap;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.net.URI;
+
+import org.glassfish.grizzly.http.server.HttpServer;
+import org.glassfish.jersey.grizzly2.httpserver.GrizzlyHttpServerFactory;
+import org.glassfish.jersey.server.ResourceConfig;
+
+public class LeapAPIMock {
+
+    private static final Logger log = LoggerFactory.getLogger(LeapAPIMock.class);
+    
+    /**
+     * Starts Grizzly HTTP server exposing JAX-RS resources defined in this application.
+     *
+     * @return Grizzly HTTP server.
+     */
+    private static HttpServer startServer() {
+        final ResourceConfig rc = new ResourceConfig().packages("io.pravega.sensor.collector.leap");
+        
+        return GrizzlyHttpServerFactory.createHttpServer(URI.create("http://0.0.0.0:8085"),rc);
+    }
+
+    public static void main(String[] args) throws Exception {
+
+        final HttpServer server = startServer();
+        log.info("Server running at http://0.0.0.0:8085");
+        // String val = authenticateMethod();
+
+        // LeapHandler httpHandler = new LeapHandler();
+        // server.getServerConfiguration().addHttpHandler(httpHandler);
+
+    }
+
+    
+
+
+
+}
