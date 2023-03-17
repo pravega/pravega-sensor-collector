44a817a1f4c1b59ee92ce1fb54e21d386af710ed
apoorva918 <apoorva.shivpuriya918@gmail.com>
2023-03-17 15:29:12 -0700

    Added code for ingesting parquet files

---
+++ pravega-sensor-collector/src/main/java/io/pravega/sensor/collector/parquet/EventGenerator.java
@@ -0,0 +1,175 @@
+
+package io.pravega.sensor.collector.parquet;
+
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.util.List;
+import java.util.function.Consumer;
+import java.util.stream.Collectors;
+
+import com.fasterxml.jackson.databind.JsonNode;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.node.ArrayNode;
+import com.fasterxml.jackson.databind.node.ObjectNode;
+import com.google.common.io.CountingInputStream;
+
+import org.apache.avro.Schema;
+import org.apache.avro.SchemaBuilder;
+import org.apache.avro.generic.GenericData;
+import org.apache.avro.generic.GenericRecord;
+import org.apache.commons.io.IOUtils;
+import org.apache.commons.lang3.tuple.ImmutablePair;
+import org.apache.commons.lang3.tuple.Pair;
+import org.apache.hadoop.conf.Configuration;
+import org.apache.hadoop.fs.Path;
+import org.apache.parquet.avro.AvroParquetReader;
+import org.apache.parquet.avro.AvroSchemaConverter;
+import org.apache.parquet.avro.AvroReadSupport;
+import org.apache.parquet.hadoop.api.InitContext;
+import org.apache.parquet.hadoop.api.ReadSupport;
+import org.apache.parquet.schema.Type;
+
+import org.apache.parquet.format.converter.ParquetMetadataConverter;
+import org.apache.parquet.hadoop.ParquetFileReader;
+import org.apache.parquet.hadoop.ParquetReader;
+import org.apache.parquet.hadoop.util.HadoopInputFile;
+import org.apache.parquet.schema.MessageType;
+import org.apache.parquet.schema.PrimitiveType;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class EventGenerator {
+    private static final Logger log = LoggerFactory.getLogger(EventGenerator.class);
+
+    private final String routingKey;
+    private final int maxRecordsPerEvent;
+    private final ObjectNode eventTemplate;
+    private final ObjectMapper mapper;
+
+    public EventGenerator(String routingKey, int maxRecordsPerEvent, ObjectNode eventTemplate, ObjectMapper mapper) {
+        this.routingKey = routingKey;
+        this.maxRecordsPerEvent = maxRecordsPerEvent;
+        this.eventTemplate = eventTemplate;
+        this.mapper = mapper;
+    }
+
+    public static EventGenerator create(String routingKey, int maxRecordsPerEvent, String eventTemplateStr, String writerId) {
+        try {
+            final ObjectMapper mapper = new ObjectMapper();
+            final ObjectNode eventTemplate = (ObjectNode) mapper.readTree(eventTemplateStr);
+            eventTemplate.put("WriterId", writerId);
+            return new EventGenerator(routingKey, maxRecordsPerEvent, eventTemplate, mapper);
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    public static EventGenerator create(String routingKey, int maxRecordsPerEvent) throws IOException {
+        return create(routingKey, maxRecordsPerEvent, "{}", "MyWriterId");
+    }
+
+
+    // PARQUET TO JSON
+
+    /**
+     * @param inputStream
+     * @param firstSequenceNumber
+     * @return next sequence number, end offset
+     */
+    protected Pair<Long, Long> generateEventsFromInputStream(CountingInputStream inputStream, long firstSequenceNumber, Consumer<PravegaWriterEvent> consumer) throws IOException {
+        File tempFile = File.createTempFile("temp", ".parquet");
+        FileOutputStream outputStream = new FileOutputStream(tempFile);
+        IOUtils.copy(inputStream,outputStream);
+        outputStream.close();
+        Path tempFilePath = new Path(tempFile.toString());
+        Configuration conf = new Configuration();
+        
+        MessageType schema = ParquetFileReader.readFooter(HadoopInputFile.fromPath(tempFilePath, conf),ParquetMetadataConverter.NO_FILTER).getFileMetaData().getSchema();        
+        //Modifying field names in extracted schema (removing special characters) 
+        List<Type> fields = schema.getFields().stream()
+                                .map(field -> new PrimitiveType(field.getRepetition(),  
+                                                // field.getType(), 
+                                                PrimitiveType.PrimitiveTypeName.valueOf(((PrimitiveType) field).getPrimitiveTypeName().toString()),
+                                                field.getName()
+                                .replaceAll("[^A-Za-z0-9_]+", "_")))
+                                .collect(Collectors.toList());
+        MessageType modifiedSchema = new MessageType(schema.getName(), fields);
+        
+        Schema avroSchema = new AvroSchemaConverter(conf).convert(modifiedSchema);
+
+        // //Creating new schema from avroSchema with modified field names (field names must begin with alphabets or '_')
+        // SchemaBuilder.FieldAssembler<Schema> assembler = SchemaBuilder.record("avroRecordName").fields();
+        // for (Schema.Field field : avroSchema.getFields()){
+        //     String fieldName= field.name().replaceAll("[^A-Za-z0-9_]", "_").replaceAll("^[^a-zA-Z_]", "_");
+        //     Schema newFieldSchema= Schema.create(field.schema().getType());
+        //     assembler.name(fieldName).type(newFieldSchema).noDefault();
+        // }
+        // Schema newAvroSchema = assembler.endRecord();
+        
+        final AvroParquetReader.Builder<GenericRecord> builder = AvroParquetReader.<GenericRecord>builder(tempFilePath);
+        AvroReadSupport.setAvroReadSchema(conf, avroSchema);        
+        final ParquetReader<GenericRecord> reader = builder.withDataModel(GenericData.get()).withConf(conf).build();
+
+        long nextSequenceNumber = firstSequenceNumber;
+        int numRecordsInEvent = 0;
+        ObjectNode jsonEvent = null;
+        GenericRecord record;
+        while ((record = reader.read())!=null) {
+            if (numRecordsInEvent >= maxRecordsPerEvent) {
+                consumer.accept(new PravegaWriterEvent(routingKey, nextSequenceNumber, mapper.writeValueAsBytes(jsonEvent)));
+                nextSequenceNumber++;
+                jsonEvent = null;
+                numRecordsInEvent = 0;
+            }
+            if (jsonEvent == null) {
+                jsonEvent = eventTemplate.deepCopy();
+            }
+            for (Schema.Field field : record.getSchema().getFields()){
+                // log.info("Parquet File record : {}", record.toString());
+                String key =  field.name();
+                String value;
+                if(record.get(key)!= null)
+                    value = record.get(key).toString();
+                else
+                    value = null;
+                addValueToArray(jsonEvent, key, value);
+            }
+            numRecordsInEvent++;
+        }
+        if (jsonEvent != null) {
+            consumer.accept(new PravegaWriterEvent(routingKey, nextSequenceNumber, mapper.writeValueAsBytes(jsonEvent)));
+            nextSequenceNumber++;
+        }
+        final long endOffset = inputStream.getCount();
+        tempFile.delete();
+        return new ImmutablePair<>(nextSequenceNumber, endOffset);
+    }
+
+    protected JsonNode stringValueToJsonNode(String s) {
+        // TODO: convert timestamp
+        try {
+            return mapper.getNodeFactory().numberNode(Long.parseLong(s));
+        } catch (NumberFormatException ignored) {}
+        try {
+            return mapper.getNodeFactory().numberNode(Double.parseDouble(s));
+        } catch (NumberFormatException ignored) {}
+        return mapper.getNodeFactory().textNode(s);
+    }
+
+    protected void addValueToArray(ObjectNode objectNode, String key, String value) {
+        if(value == null){
+            objectNode.putNull(key);
+        }
+        else{    
+            final JsonNode node = objectNode.get(key);
+            final JsonNode valueNode = stringValueToJsonNode(value);
+            if (node instanceof ArrayNode ) {
+                ((ArrayNode) node).add(valueNode);
+            } else {
+                objectNode.putArray(key).add(valueNode);
+            }
+        }
+    }
+
+}
---
+++ pravega-sensor-collector/src/main/java/io/pravega/sensor/collector/parquet/ParquetFileIngestService.java
@@ -0,0 +1,146 @@
+/**
+ * Copyright (c) Dell Inc., or its subsidiaries. All Rights Reserved.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ */
+package io.pravega.sensor.collector.parquet;
+
+import java.util.concurrent.Executors;
+import java.util.concurrent.ScheduledExecutorService;
+import java.util.concurrent.ScheduledFuture;
+import java.util.concurrent.ThreadFactory;
+import java.util.concurrent.TimeUnit;
+
+import com.google.common.util.concurrent.ThreadFactoryBuilder;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import io.pravega.client.EventStreamClientFactory;
+import io.pravega.sensor.collector.DeviceDriver;
+import io.pravega.sensor.collector.DeviceDriverConfig;
+
+
+public class ParquetFileIngestService extends DeviceDriver{
+    private static final Logger log = LoggerFactory.getLogger(ParquetFileIngestService.class);
+    
+    private static final String FILE_SPEC_KEY = "FILE_SPEC";
+    private static final String DELETE_COMPLETED_FILES_KEY = "DELETE_COMPLETED_FILES";
+    private static final String DATABASE_FILE_KEY = "DATABASE_FILE";
+    private static final String EVENT_TEMPLATE_KEY = "EVENT_TEMPLATE";
+    private static final String SAMPLES_PER_EVENT_KEY = "SAMPLES_PER_EVENT";
+    private static final String INTERVAL_MS_KEY = "INTERVAL_MS";
+
+    private static final String SCOPE_KEY = "SCOPE";
+    private static final String STREAM_KEY = "STREAM";
+    private static final String ROUTING_KEY_KEY = "ROUTING_KEY";
+    private static final String EXACTLY_ONCE_KEY = "EXACTLY_ONCE";
+    private static final String TRANSACTION_TIMEOUT_MINUTES_KEY = "TRANSACTION_TIMEOUT_MINUTES";
+
+    private final ParquetFileProcessor processor;
+    private final ScheduledExecutorService executor;
+    private ScheduledFuture<?> task;
+
+    public ParquetFileIngestService(DeviceDriverConfig config){
+        super(config);
+        final ParquetFileConfig parquetFileConfig = new ParquetFileConfig(
+                getDatabaseFileName(),
+                getFileSpec(),
+                getRoutingKey(),
+                getStreamName(),
+                getEventTemplate(),
+                getSamplesPerEvent(),
+                getDeleteCompletedFiles(),
+                getExactlyOnce(),
+                getTransactionTimeoutMinutes());
+        log.info("Parquet File Ingest Config: {}", parquetFileConfig);
+        final String scopeName = getScopeName();
+        log.info("Scope: {}", scopeName);
+        createStream(scopeName, getStreamName());
+
+        final EventStreamClientFactory clientFactory = getEventStreamClientFactory(scopeName);
+        processor = ParquetFileProcessor.create(parquetFileConfig, clientFactory);
+        ThreadFactory namedThreadFactory = new ThreadFactoryBuilder().setNameFormat(
+                ParquetFileIngestService.class.getSimpleName() + "-" + config.getInstanceName() + "-%d").build();
+        executor = Executors.newScheduledThreadPool(1, namedThreadFactory);
+
+    }
+
+    String getFileSpec() {
+        return getProperty(FILE_SPEC_KEY);
+    }
+
+    boolean getDeleteCompletedFiles() {
+        return Boolean.parseBoolean(getProperty(DELETE_COMPLETED_FILES_KEY, Boolean.toString(true)));
+    }
+
+    String getDatabaseFileName() {
+        return getProperty(DATABASE_FILE_KEY);
+    }
+
+    String getEventTemplate() {
+        return getProperty(EVENT_TEMPLATE_KEY, "{}");
+    }
+
+    int getSamplesPerEvent() {
+        return Integer.parseInt(getProperty(SAMPLES_PER_EVENT_KEY, Integer.toString(100)));
+    }
+
+    long getIntervalMs() {
+        return Long.parseLong(getProperty(INTERVAL_MS_KEY, Long.toString(10000)));
+    }
+
+    String getScopeName() {
+        return getProperty(SCOPE_KEY);
+    }
+
+    String getStreamName() {
+        return getProperty(STREAM_KEY);
+    }
+
+    protected String getRoutingKey() {
+        return getProperty(ROUTING_KEY_KEY, "");
+    }
+
+    boolean getExactlyOnce() {
+        return Boolean.parseBoolean(getProperty(EXACTLY_ONCE_KEY, Boolean.toString(true)));
+    }
+
+    /**
+     * This time duration must not exceed the controller property controller.transaction.maxLeaseValue (milliseconds).
+     */
+    double getTransactionTimeoutMinutes() {
+        return Double.parseDouble(getProperty(TRANSACTION_TIMEOUT_MINUTES_KEY, Double.toString(18.0 * 60.0)));
+    }
+
+    protected void ingestParquetFiles() {
+        log.info("ingestParquetFiles: BEGIN");
+        try {
+            processor.ingestParquetFiles();
+        } catch (Exception e) {
+            log.error("Error", e);
+            // Continue on any errors. We will retry on the next iteration.
+        }
+        log.info("ingestParquetFiles: END");
+    }
+
+    @Override
+    protected void doStart() {
+        task = executor.scheduleAtFixedRate(
+                this::ingestParquetFiles,
+                0,
+                getIntervalMs(),
+                TimeUnit.MILLISECONDS);
+        notifyStarted();        
+    }
+
+    @Override
+    protected void doStop() {
+        task.cancel(false);        
+    }
+
+}
