694a39dc0fb796785ff280f6d416bedc8fc2912d
apoorva918 <apoorva.shivpuriya918@gmail.com>
2023-08-09 17:50:15 -0700

    code leanup

--- pravega-sensor-collector/src/main/java/io/pravega/sensor/collector/parquet/ParquetFileProcessor.java
+++ pravega-sensor-collector/src/main/java/io/pravega/sensor/collector/parquet/ParquetFileProcessor.java
@@ -24,8 +24,6 @@ import java.util.List;
 import java.util.Optional;
 import java.util.Set;
 import java.util.UUID;
-import java.util.stream.Collectors;
-import java.util.stream.StreamSupport;
 
 import com.google.common.io.CountingInputStream;
 
