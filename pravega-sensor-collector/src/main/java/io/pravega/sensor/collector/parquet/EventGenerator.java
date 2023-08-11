9e72b2bb1c06acac4cb6374a3577eee1e48c37e1
apoorva918 <apoorva.shivpuriya918@gmail.com>
2023-08-11 13:00:48 -0700

    code cleanup

--- pravega-sensor-collector/src/main/java/io/pravega/sensor/collector/parquet/EventGenerator.java
+++ pravega-sensor-collector/src/main/java/io/pravega/sensor/collector/parquet/EventGenerator.java
@@ -10,42 +10,18 @@
  */
 package io.pravega.sensor.collector.parquet;
 
-import java.io.File;
-import java.io.FileOutputStream;
 import java.io.IOException;
-import java.nio.ByteBuffer;
-import java.util.HashMap;
-import java.util.List;
 import java.util.function.Consumer;
-import java.util.stream.Collectors;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.fasterxml.jackson.databind.node.ObjectNode;
-import com.google.common.io.ByteStreams;
 import com.google.common.io.CountingInputStream;
 
-import org.apache.avro.Schema;
-import org.apache.avro.generic.GenericData;
-import org.apache.avro.generic.GenericRecord;
-import org.apache.commons.io.IOUtils;
 import org.apache.commons.lang3.tuple.ImmutablePair;
 import org.apache.commons.lang3.tuple.Pair;
-import org.apache.hadoop.conf.Configuration;
-import org.apache.hadoop.fs.Path;
-import org.apache.parquet.avro.AvroParquetReader;
-import org.apache.parquet.avro.AvroReadSupport;
-import org.apache.parquet.avro.AvroSchemaConverter;
-import org.apache.parquet.format.converter.ParquetMetadataConverter;
-import org.apache.parquet.hadoop.ParquetFileReader;
-import org.apache.parquet.hadoop.ParquetReader;
-import org.apache.parquet.hadoop.util.HadoopInputFile;
-import org.apache.parquet.schema.MessageType;
-import org.apache.parquet.schema.PrimitiveType;
-import org.apache.parquet.schema.Type;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
-import io.pravega.common.util.ByteArraySegment;
 
 public class EventGenerator {
     private static final Logger log = LoggerFactory.getLogger(EventGenerator.class);
@@ -87,8 +63,8 @@ public class EventGenerator {
     protected Pair<Long, Long> generateEventsFromInputStream(CountingInputStream inputStream, long firstSequenceNumber, Consumer<PravegaWriterEvent> consumer) throws IOException {
         
         long nextSequenceNumber = firstSequenceNumber;
-        try{
-            byte[] byteArray = IOUtils.toByteArray(inputStream);       
+        try{    
+            byte[] byteArray = inputStream.readAllBytes();
 
             consumer.accept(new PravegaWriterEvent(routingKey, nextSequenceNumber, byteArray));
             nextSequenceNumber++;			        
