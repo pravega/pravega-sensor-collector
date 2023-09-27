22f3877f7d776533654a586b6d618283250fefdb
apoorva918 <apoorva.shivpuriya918@gmail.com>
2023-09-27 13:38:33 -0700

    added condition for wildcard

--- scripts/get-certs.sh
+++ scripts/get-certs.sh
@@ -15,14 +15,31 @@ SDP_DOMAIN=${SDP_DOMAIN:-sdp.sdp-demo.org}
 kubectl get secret ${NAMESPACE}-ext-pravega -n ${NAMESPACE} -o jsonpath="{.data.keycloak\.json}" |base64 -d ;echo > ~/keycloak.json
 chmod go-rw ~/keycloak.json
 
-kubectl get secret pravega-controller-tls -n nautilus-pravega -o jsonpath="{.data.tls\.crt}" | base64 --decode > ~/pravega.crt
-chmod go-rw ~/pravega.crt
+# Check if pravega-controller-tls secret exists
+if kubectl get secret pravega-controller-tls -n nautilus-pravega &> /dev/null; then
+    kubectl get secret pravega-controller-tls -n nautilus-pravega -o jsonpath="{.data.tls\.crt}" | base64 --decode > ~/pravega.crt
+    chmod go-rw ~/pravega.crt
+else
+    echo "Secret pravega-controller-tls not found, extracting cluster-wildcard-tls-secret"
+    kubectl get secret cluster-wildcard-tls-secret -n nautilus-pravega -o jsonpath="{.data.tls\.crt}" | base64 --decode > ~/pravegawc.crt
+    chmod go-rw ~/pravegawc.crt
+fi
 
-kubectl get secret keycloak-tls -n nautilus-system -o jsonpath="{.data.tls\.crt}" | base64 --decode > ~/keycloak.crt
-chmod go-rw ~/keycloak.crt
+# Check if keycloak-tls secret exists
+if kubectl get secret keycloak-tls -n nautilus-system &> /dev/null; then
+    kubectl get secret keycloak-tls -n nautilus-system -o jsonpath="{.data.tls\.crt}" | base64 --decode > ~/keycloak.crt
+    chmod go-rw ~/keycloak.crt
+else
+    echo "Secret keycloak-tls not found, extracting cluster-wildcard-tls-secret"
+    kubectl get secret cluster-wildcard-tls-secret -n nautilus-system -o jsonpath="{.data.tls\.crt}" | base64 --decode > ~/keycloakwc.crt
+    chmod go-rw ~/keycloakwc.crt
+fi
 
 kubectl get secret pravega-tls -n nautilus-pravega -o jsonpath="{.data.tls\.crt}" | base64 --decode > ~/pravegaAll.crt
 chmod go-rw ~/pravegaAll.crt 
 
 echo | openssl s_client -showcerts -servername keycloak.${SDP_DOMAIN} -connect keycloak.${SDP_DOMAIN}:443 2>/dev/null |  sed -ne '/-BEGIN CERTIFICATE-/,/-END CERTIFICATE-/p' > ~/keycloakIng.crt
 chmod go-rw ~/keycloakIng.crt 
+
+echo "Extracted files"
+ls -l ~/ | grep -E '\.json|\.crt'
