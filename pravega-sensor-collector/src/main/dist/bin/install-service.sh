549009f79d5d21133e6d8f347a7a40a11d74b305
Abhin Balur <Abhin_Balur@dell.com>
2024-01-22 09:01:50 -0800

    Watchdog draft v1
    Signed-off-by: Abhin Balur <Abhin_Balur@dell.com>

--- pravega-sensor-collector/src/main/dist/bin/install-service.sh
+++ pravega-sensor-collector/src/main/dist/bin/install-service.sh
@@ -8,14 +8,21 @@
 #
 #     http://www.apache.org/licenses/LICENSE-2.0
 #
+#TODO: split this into 2 functions, start-watchdog and start-psc
 set -x
 ROOT_DIR=$(readlink -f $(dirname $0)/..)
 SERVICE_NAME=${SERVICE_NAME:-$(basename ${ROOT_DIR})}
 echo Installing service ${SERVICE_NAME} located in ${ROOT_DIR}.
 sed -e "s:\${ROOT_DIR}:${ROOT_DIR}:g" ${ROOT_DIR}/etc/pravega-sensor-collector-TEMPLATE.service \
     > ${ROOT_DIR}/etc/${SERVICE_NAME}.service
+sed -e "s:\${ROOT_DIR}:${ROOT_DIR}:g" ${ROOT_DIR}/etc/psc-watchdog-TEMPLATE.service \
+    > ${ROOT_DIR}/etc/psc-watchdog.service
 systemctl stop ${SERVICE_NAME}.service
+systemctl stop psc-watchdog.service
 ln -svf ${ROOT_DIR}/etc/${SERVICE_NAME}.service /etc/systemd/system/${SERVICE_NAME}.service
+ln -svf ${ROOT_DIR}/etc/psc-watchdog.service /etc/systemd/system/psc-watchdog.service
 systemctl daemon-reload
 systemctl enable ${SERVICE_NAME}.service
+systemctl enable psc-watchdog.service
 systemctl start ${SERVICE_NAME}.service
+systemctl start psc-watchdog.service
