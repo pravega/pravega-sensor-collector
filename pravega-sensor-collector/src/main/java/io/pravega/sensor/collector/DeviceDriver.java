e2668d043753a1efd3eaa9ee4585fae232f7ee23
Amit-Singh40 <amit.singh30@dell.com>
2024-01-23 13:42:02 +0530

    Resolving conflicts
    Signed-off-by: Amit-Singh40 <amit.singh30@dell.com>

--- pravega-sensor-collector/src/main/java/io/pravega/sensor/collector/DeviceDriver.java
+++ pravega-sensor-collector/src/main/java/io/pravega/sensor/collector/DeviceDriver.java
@@ -31,7 +31,7 @@ public abstract class DeviceDriver extends AbstractService implements AutoClosea
     private static final String CREATE_SCOPE_KEY = "CREATE_SCOPE";
 
     public DeviceDriver(DeviceDriverConfig config) {
-        this.config = Preconditions.checkNotNull(config, "config");
+        this.config = Preconditions.checkNotNull(config, "deviceDriverConfig");
         LOGGER.info("Create Scope: {}", isCreateScope());
     }
 
