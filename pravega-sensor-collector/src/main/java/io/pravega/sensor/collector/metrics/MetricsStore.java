dea286f4d75952624582e6f02e2741c1348d3bbf
Abhin Balur <Abhin_Balur@dell.com>
2024-02-29 04:06:47 -0800

    Handling comment
    Signed-off-by: Abhin Balur <Abhin_Balur@dell.com>

--- pravega-sensor-collector/src/main/java/io/pravega/sensor/collector/metrics/MetricsStore.java
+++ pravega-sensor-collector/src/main/java/io/pravega/sensor/collector/metrics/MetricsStore.java
@@ -17,6 +17,8 @@ import com.google.common.collect.ImmutableMap;
  * Simple Store holding metric data centrally.
  */
 public class MetricsStore {
+    private static ObjectMapper mapper = new ObjectMapper();
+    
     private static final ImmutableMap<String, Metric> metricStore = ImmutableMap.<String, Metric>builder()
             .put(MetricNames.PSC_FILES_PROCESSED_GAUGE, new Gauge())
             .put(MetricNames.PSC_FILES_DELETED_GAUGE, new Gauge())
@@ -36,7 +38,6 @@ public class MetricsStore {
      * json string.
      */
     public static String getMetricsAsJson() throws JsonProcessingException {
-        ObjectMapper mapper = new ObjectMapper();
         return mapper.writerWithDefaultPrettyPrinter()
                 .writeValueAsString(metricStore);
     }
